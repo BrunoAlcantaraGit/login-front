689482cb669e1fc613b5073d2de4b328156f601b
BrunoAlcantara <brunodeoliveiraalcantara@gmail.com>
2025-03-22 23:32:13 -0300

    Adição de estilização CSS global;
    Adição de dives e container para estilização do HTML login-default
    
    CSS componente Login Defaut
    Deixando preenchimento

--- src/app/components/default-login/default-login.component.ts
+++ src/app/components/default-login/default-login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './default-login.component.scss'
 })
 export class DefaultLoginComponent {
-
+@Input() title = '';
+@Input() primaryButtonText = '';
+@Input() secondaryButtonText = '';
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 import { DefaultLoginComponent } from '../../components/default-login/default-login.component';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [DefaultLoginComponent],
+  imports:
+  [DefaultLoginComponent,
+  ReactiveFormsModule,
+
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+loginForm !: FormGroup;
+@Output() loginEvent = new EventEmitter();
+
+constructor(
+  private formBuilder:FormBuilder)
+{
+  this.loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+}
+
+
 
 }
